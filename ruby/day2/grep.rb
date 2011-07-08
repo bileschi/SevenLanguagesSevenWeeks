e9ba1e2100c7324e2c07f9fa5e8ddff6fda173df
Stanley Bileschi <stanleybileschi@smb-mbp.local>
2011-07-08 01:53:24 -0400

    grep works

--- ruby/day2/grep.rb
+++ ruby/day2/grep.rb
@@ -2,15 +2,21 @@
 #this file implements a grep like tool in ruby
 
 def usage
-   puts "USAGE: grep.rb filename"
+   puts "USAGE: grep.rb filename substr"
 end
 
-if ARGV.length != 1
+if ARGV.length != 2
    usage
 else
-  ARGV.each do|a|
-    puts "Argument: #{a}"
-  end
+   File.open(ARGV[0], "r") do |aFile|
+      aFile.each_line do |line| 
+          if line =~ Regexp.new(ARGV[1]) 
+             puts "Got #{line.dump}" 
+          end
+          # ... process the line
+      end
+      # ... process the file
+   end
 end
 
 
