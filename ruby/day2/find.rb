4fa2b154cf577dc5d39966dc43c7ddf5203a3f85
Stanley Bileschi <stanleybileschi@smb-mbp.local>
2011-07-26 01:53:12 -0400

    playing with files

--- ruby/day2/find.rb
+++ ruby/day2/find.rb
@@ -1,2 +1,25 @@
 # how to access files with and without code blocks
 
+# opening a file without a code block
+tempfilename = "./temp.txt"
+File.exists?(tempfilename)
+myFile = File.open(tempfilename,'w') # returns an IO
+myFile.closed?
+myFile.close
+File.exists?(tempfilename)
+File.delete(tempfilename) # returns 1
+if File.exists?(tempfilename)
+	puts "i already deleted this"
+else
+	puts "it is gone"
+end
+
+#print my own comments
+myselfAsCode = File.open('./find.rb');
+myselfAsCode.each_line do |line|
+	if line =~ Regexp.new("#.*$") 
+	 puts "Got #{\1}" 
+	end
+end
+
+
