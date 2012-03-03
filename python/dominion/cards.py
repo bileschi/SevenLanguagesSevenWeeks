22dcd73a63717a08390b0d657b104358e5abd560
Stan Bileschi <sbileschi@dataxu.com>
2012-03-03 11:52:38 -0500

    split cards and pile.  Moved to nose tests

--- python/dominion/cards.py
+++ python/dominion/cards.py
@@ -1,9 +1,5 @@
 #!/usr/bin/env python
 
-# classes for card and pile
-# a pile is an ordered collection of cards with an easy shuffling operation
-
-from random import shuffle
 from card_prototypes import card_prototypes
 
 class Card(object):
@@ -12,86 +8,52 @@ class Card(object):
 		self._properties = card_prototypes[name];
 	def __str__(self):
 		return self._name
+	def is_treasure(self):
+		return self._properties.is_treasure
+	def is_action(self):
+		return self._properties.is_action
+	def is_victory(self):
+		return self._properties.is_victory
+	def is_curse(self):
+		return self._properties.is_curse
 
-class Pile(object):
-	def __init__(self, name='', count=0):
-		self._cards = []
-		if count > 0:
-			self.add_new_card_to_pile(name, count)
-	def add_card_to_pile(self, card):
-		if card is not None:
-			self._cards.append(card)
-	def add_new_card_to_pile(self, name, count=1):
-		self._cards.extend([Card(name) for i in range(0,count)])
-	def add_others_to_pile(self, other_pile):
-		""" takes all the cards out of other_pile and adds them in order to this pile.
-		should work with other_pile = Pile instance or other_pile = List(Card) """
-		try:
-			self._cards.extend(other_pile._cards)
-			other_pile.empty()
-		except AttributeError:
-			self._cards.extend(other_pile)
-			other_pile = []
-	def count_any_card(self):
-		return len(self._cards)
-	def count_specific_card(self, card_name):
-		c = 0
-		for card in self._cards:
-			c += 1 if card._name == card_name else 0
-		return c
-	def empty(self):
-		""" removes all cards from this pile """
-		self._cards = []
-	def pop(self):
-		"returns a single card off the top of the deck"
-		return self._cards.pop()
-	def pull_first_instance_of(self, card_name):
-		"""removes first instance of 'card' from pile_name.  If there aren't any, returns None"""
-		for i, card in enumerate(self._cards):
-			if card_name == card._name:
-				return self._cards.pop(i)
-		return None
-	def shuffle(self):
-		"randomly reorders cards in pile"
-		shuffle(self._cards)
-	def take_top_n(self, n=1):
-		"returns a list of n cards off the top of the deck.  The list is not reordered"
-		top_n_cards = self._cards[-n:]
-		self._cards = self.cards[0:-n]
-		return top_n_cards
-	def pretty_print(self):
-		print str(self)
-		for card in self._cards:
-			print card
-	def __str__(self):
-		suffix = "" if len(self._cards) == 1 else "s"
-		return "pile with " + str(len(self._cards)) + " card" + suffix
-	def __len__(self):
-		return len(self._cards)
+################ Unit Tests ###########
+
+def test_making_undefined_card_raises_key_error():
+	non_card_name = 'this_is_not_a_real_card_name'
+	assert non_card_name not in card_prototypes
+	try:
+		non_card = Card(non_card_name) # instantiate a non card
+		assert False 
+	except KeyError as e:
+		return
+	assert False
+
+def test_making_defined_cards():
+	for card_name in card_prototypes.keys():
+		card = Card(card_name)
 
 
-################ Definition Complete ###########
+def test_referencing_properties():
+	for card_name in card_prototypes.keys():
+		card = Card(card_name)
+		assert(type(card._properties).__name__ ==  'card_prototype_data') 
 
+################ Usage Examples ###########
 
-# Testing.
 if __name__ == "__main__":
-	supply = {}
-	supply['copper'] = Pile('copper', 30 )
-	supply['estate'] = Pile('estate', 30)
-	supply['fake'] = Pile('fake', 1)
-	print(str(supply))
-	print(str(supply['copper']))
-	print(str(supply['estate']))
-	print(str(supply['fake']))
-	hand = Pile('copper', 7)
-	hand.add_new_card_to_pile('estate', 3)
-	print '--------------'
-	hand.pretty_print()
-	hand.shuffle()
-	print '--------------'
-	hand.pretty_print()
-	print "number of copper: " + str(hand.count_specific_card('copper'))
-	print "number of estate: " + str(hand.count_specific_card('estate'))
+	card = Card('copper')
+	print card._properties.is_treasure
+	for card_name in card_prototypes.keys():
+		card = Card(card_name)
+		if card.is_treasure():
+			print card_name + " is a treasure card costing " + str(card._properties.purchase_cost)
+		if card.is_action():
+			print card_name + " is an action card costing " + str(card._properties.purchase_cost)
+		if card.is_victory():
+			print card_name + " is a victory card costing " + str(card._properties.purchase_cost)
+		if card.is_curse():
+			print card_name + " is a curse card costing " + str(card._properties.purchase_cost)
 
 
 
---
+++ python/dominion/pile.py
@@ -0,0 +1,82 @@
+#!/usr/bin/env python
+
+from random import shuffle
+from cards import Card
+
+class Pile(object):
+	"""a pile is an ordered collection of cards with an easy shuffling operation
+	   it also has utility for counting specific cards etc.""" 
+	def __init__(self, name='', count=0):
+		self._cards = []
+		if count > 0:
+			self.add_new_card_to_pile(name, count)
+	def add_card_to_pile(self, card):
+		if card is not None:
+			self._cards.append(card)
+	def add_new_card_to_pile(self, name, count=1):
+		self._cards.extend([Card(name) for i in range(0,count)])
+	def add_others_to_pile(self, other_pile):
+		""" takes all the cards out of other_pile and adds them in order to this pile.
+		should work with other_pile = Pile instance or other_pile = List(Card) """
+		try:
+			self._cards.extend(other_pile._cards)
+			other_pile.empty()
+		except AttributeError:
+			self._cards.extend(other_pile)
+			other_pile = []
+	def count_any_card(self):
+		return len(self._cards)
+	def count_specific_card(self, card_name):
+		c = 0
+		for card in self._cards:
+			c += 1 if card._name == card_name else 0
+		return c
+	def empty(self):
+		""" removes all cards from this pile """
+		self._cards = []
+	def pop(self):
+		"returns a single card off the top of the deck"
+		return self._cards.pop()
+	def pull_first_instance_of(self, card_name):
+		"""removes first instance of 'card' from pile_name.  If there aren't any, returns None"""
+		for i, card in enumerate(self._cards):
+			if card_name == card._name:
+				return self._cards.pop(i)
+		return None
+	def shuffle(self):
+		"randomly reorders cards in pile"
+		shuffle(self._cards)
+	def take_top_n(self, n=1):
+		"returns a list of n cards off the top of the deck.  The list is not reordered"
+		top_n_cards = self._cards[-n:]
+		self._cards = self.cards[0:-n]
+		return top_n_cards
+	def pretty_print(self):
+		print str(self)
+		for card in self._cards:
+			print card
+	def __str__(self):
+		suffix = "" if len(self._cards) == 1 else "s"
+		return "pile with " + str(len(self._cards)) + " card" + suffix
+	def __len__(self):
+		return len(self._cards)
+
+
+if __name__ == '__main__':
+	supply = {}
+	supply['copper'] = Pile('copper', 30 )
+	supply['estate'] = Pile('estate', 30)
+	# supply['fake'] = Pile('fake', 1)
+	print(str(supply))
+	print(str(supply['copper']))
+	print(str(supply['estate']))
+	# print(str(supply['fake']))
+	hand = Pile('copper', 7)
+	hand.add_new_card_to_pile('estate', 3)
+	print '--------------'
+	hand.pretty_print()
+	hand.shuffle()
+	print '--------------'
+	hand.pretty_print()
+	print "number of copper: " + str(hand.count_specific_card('copper'))
+	print "number of estate: " + str(hand.count_specific_card('estate'))
