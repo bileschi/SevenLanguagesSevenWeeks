64a2798a97f7b392456b2cd58ad42dc3b50f7540
Stan Bileschi <sbileschi@dataxu.com>
2012-03-12 00:28:31 -0400

    working toward a more general map coloring procedure

--- haskell/day1/mapcolor.hs
+++ haskell/day1/mapcolor.hs
@@ -1,4 +1,5 @@
 -- map coloring problem.  Assign colors to states such that no two adjacent states have the same color.
+import Data.List
 
 -- Tennesee
 -- Mississippi
@@ -45,3 +46,40 @@ my_colors = [ [c1, c2, c3, c4, c5] |
 
 
 color_states = zip states (head my_colors)
+
+---------------- Trying a second approach.
+
+-- l is a list, and constraints is a list of integer tuples
+-- function returns true iff for each tuple (idx1, idx2) l[idx1] /= l[idx2]
+-- this is implemented as a tail recursive function
+check_elements_neq :: (Eq a) => [a] -> [(Int, Int)] -> Bool
+check_elements_neq l [] = True -- no constraints returns true
+ -- check the first constraint and recurse
+check_elements_neq l ((idx1, idx2):rest) = ((l !! idx1) /= (l !! idx2)) && (check_elements_neq l rest)
+
+-- builds integral constraints from the states and state-edges data
+index_constraints :: (Eq a) => [a] -> [(a,a)] -> [(Maybe Int, Maybe Int)]
+index_constraints v [] = []
+index_constraints v ((n1, n2):rest) = ((elemIndex n1 v), (elemIndex n2 v)) : (index_constraints v rest)
+
+-- removes data errors stemming from state-borders to undefined states
+filter_constraints :: [(Maybe t0, Maybe t1)] -> [(t0, t1)]
+filter_constraints [] = []
+filter_constraints ((Nothing, _):rest) = filter_constraints rest
+filter_constraints ((_, Nothing):rest) = filter_constraints rest
+filter_constraints ((Just a, Just b):rest) = (a,b):(filter_constraints rest)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
